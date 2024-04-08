837dc09796c558084ebbd307314078b71e9331c9
GustavoAssuncao02 <94201266+GustavoAssuncao02@users.noreply.github.com>
2024-04-08 10:54:53 -0300

    navbar adjustments

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -2,20 +2,21 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from "../../Assets/Images/logo.png"
 import cart from "../../Assets/Images/Cart.png"
+import Styles from "../NavBar/NavBar.css"
 function NavBar() {
     return (
         <div>
-            <nav class="navbar bg-black col-12">
+            <nav class="navbar bg-black col-12 navbar-expand-lg">
                 <div class="container-fluid">
-                    <a class="navbar-brand d-flex justify-content-between align-items-center w-100">
+                    <a class="navbar-brand d-flex justify-content align-items-center  w-100">
                         <img src={logo} alt="Logo" width="110" height="76" class="d-inline-block align-text-top" />
-                        <div class="me-5 mx-auto">
+                        <div class="col-4">
                         <form class="d-flex" role="search">
-                            <input class="form-control" type="search" placeholder="Search" aria-label="Search"/>
+                            <input class="form-control " type="search" placeholder="Digite o que você procura" aria-label="Search"/>
                                 <button class="btn btn-outline-success" type="submit">Search</button>
                         </form>
                         </div>
-                        <img src={cart} alt="Logo" width="40" class="d-inline-block align-text-top me-5" />
+                        <img src={cart} alt="Logo" width="40" class="d-inline-block align-text-top" />
                     </a>
                 </div>
             </nav>
