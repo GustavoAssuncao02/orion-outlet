afec7ef68b584ae618584d3bf20796cfcdc2afa5
GustavoAssuncao02 <94201266+GustavoAssuncao02@users.noreply.github.com>
2024-04-18 11:23:37 -0300

    Create carousel component

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import "./Home.css";
 import NavBar from '../../Components/NavBar/NavBar';
+import Carousel from '../../Components/Carousel/Carousel';
 const Home = () => {
   return (
-    <NavBar/>
+    <><NavBar /><Carousel /></>
   )
 }
 
