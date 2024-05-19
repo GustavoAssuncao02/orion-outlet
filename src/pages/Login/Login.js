5cc71673b4c4f7468d94bf057de424b612ab5840
GustavoAssuncao02 <94201266+GustavoAssuncao02@users.noreply.github.com>
2024-05-19 00:56:46 -0300

    Index fixes body config, login update

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -3,11 +3,12 @@ import "../Login/Login.css";
 import Logo from "../../assets/Images/OrionOutlet/OrionOutlet.jpg";
 
 function Login() {
+  
   return (
     <div>
-      <section className="vh-100">
+      <section className="p-5">
         <div className="container-fluid h-custom">
-          <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className=" d-flex justify-content-center align-items-center">
             <div className="col-md-9 col-lg-6 col-xl-5">
               <img src={Logo} className="img-fluid logo1" alt="Sample" />
             </div>
@@ -42,7 +43,7 @@ function Login() {
                     data-mdb-button-init
                     data-mdb-ripple-init
                     className="btn btn-primary btn-lg Login-button"
-                    // onClick={(e) => hendleLogin(e)}Function to handle login
+                  // onClick={(e) => hendleLogin(e)} //Function to handle login
                   >
                     Login
                   </button>
@@ -52,6 +53,17 @@ function Login() {
           </div>
         </div>
       </section>
+
+
+      <div className="text-white bg-dark p-4">
+        <p>
+          Copyright ©2024 Gustavo e Camila. Todos os direitos reservados.
+          O conteúdo deste site, incluindo texto, imagens, gráficos e outros materiais, é protegido por leis de direitos autorais e outras leis de propriedade intelectual. O conteúdo deste site não pode ser reproduzido, distribuído, transmitido, exibido, publicado ou veiculado sem a autorização prévia por escrito de Gustavo e Camila.
+        </p>
+        <p>
+          Encontrou um problema? Entre em contato conosco, estamos sempre tentando melhorar o nosso sistema
+        </p>
+      </div>
     </div>
   );
 }
