5f0f123615f52d1535d2cd7930e2611444326ca0
Camila Carvalho <by.cmlla0107@gmail.com>
2024-05-19 01:08:15 -0300

    change the cover image of commercial

--- src/components/Commercial/Commercial.js
+++ src/components/Commercial/Commercial.js
@@ -5,26 +5,24 @@ import { AiFillInstagram } from "react-icons/ai";
 import { FaFacebook } from "react-icons/fa";
 
 function Commercial() {
+  const links = [
+    { label: "Whatsapp", url: 'https://api.whatsapp.com/send/?phone=5571996613066&text=Ol%C3%A1%21+Desejo+ver+o+cat%C3%A1logo+com+todos+os+produtos.&type=phone_number&app_absent=0'},
+    { label: "Instagram", url: 'https://www.instagram.com/orionoutlet_/'},
+    { label: "Facebook", url: 'https://www.facebook.com/oironoutlet3066'},
+  ];
   return (
     <div className="background-container">
       <div className="text-end text-white">
-        <h1>Nossas Redes Sociais</h1>
-        <div id="socialMidias">
-          <a href="https://api.whatsapp.com/send/?phone=5571996613066&text=Ol%C3%A1%21+Desejo+ver+o+cat%C3%A1logo+com+todos+os+produtos.&type=phone_number&app_absent=0">
-            <IoLogoWhatsapp />
-          </a>
-          <a href="https://www.instagram.com/orionoutlet_/">
-            <AiFillInstagram />
-          </a>
-          <a href="https://www.facebook.com/oironoutlet3066">
-            <FaFacebook />
-          </a>
+        <p>Nossas Redes Sociais</p>
+        <div className="social-midias">
+          {links.map((link, index) => (
+            <a key={index} href={link.url}>
+              {link.label === "Whatsapp" ? <IoLogoWhatsapp className="social-icon"/> : null}
+              {link.label === "Instagram" ? <AiFillInstagram className="social-icon" /> : null}
+              {link.label === "Facebook" ? <FaFacebook className="social-icon" /> : null}
+            </a>
+          ))}
         </div>
-      </div>
-      <div>
-        <h1 id="CommercialText" className="text-white">
-          Até 60% de <br></br>desconto
-        </h1>
       </div>
     </div>
   );
