08b0b0905826972cc07661943a4711e22a9529ac
GustavoAssuncao02 <94201266+GustavoAssuncao02@users.noreply.github.com>
2024-05-19 00:09:37 -0300

    Footer Update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,15 +12,15 @@ function Footer() {
           <div className="row align-items-start pt-3">
             <div className="col text-white">
               Social
-              <div id="socialMidias" className="p-4">
+              <div className="p-4"> 
                 <a href="https://api.whatsapp.com/send/?phone=5571996613066&text=Ol%C3%A1%21+Desejo+ver+o+cat%C3%A1logo+com+todos+os+produtos.&type=phone_number&app_absent=0">
-                  <IoLogoWhatsapp />
+                  <IoLogoWhatsapp className="social-midias"/>
                 </a>
                 <a href="https://www.instagram.com/orionoutlet_/">
-                  <AiFillInstagram />
+                  <AiFillInstagram className="social-midias"/>
                 </a>
                 <a href="https://www.facebook.com/oironoutlet3066">
-                  <FaFacebook />
+                  <FaFacebook className="social-midias" />
                 </a>
               </div>
             </div>
