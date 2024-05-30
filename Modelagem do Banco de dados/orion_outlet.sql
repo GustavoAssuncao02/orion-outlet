10b540d818de4ad2a4ec22a3e94435d14749f8f4
GustavoAssuncao02 <94201266+GustavoAssuncao02@users.noreply.github.com>
2024-05-30 19:50:38 -0300

    update database

--- Modelagem do Banco de dados/orion_outlet.sql
+++ Modelagem do Banco de dados/orion_outlet.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 30/05/2024 às 23:37
+-- Tempo de geração: 31/05/2024 às 00:50
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.2.4
 
@@ -24,6 +24,21 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura para tabela `address`
+--
+
+CREATE TABLE `address` (
+  `id` int(11) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `street` int(11) NOT NULL,
+  `number` int(11) NOT NULL,
+  `district` varchar(50) NOT NULL,
+  `reference` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura para tabela `descriptions`
 --
 
@@ -37,9 +52,69 @@ CREATE TABLE `descriptions` (
   `price` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `items`
+--
+
+CREATE TABLE `items` (
+  `id_items` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `image` blob NOT NULL,
+  `quantity` int(200) NOT NULL,
+  `category` varchar(45) NOT NULL,
+  `id_descriptionfk` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `orders`
+--
+
+CREATE TABLE `orders` (
+  `id_cart` int(11) NOT NULL,
+  `id_userfk` int(11) NOT NULL,
+  `id_itemsfk` int(11) NOT NULL,
+  `payment_method` varchar(45) NOT NULL,
+  `installment` int(11) NOT NULL,
+  `status` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `number` varchar(11) NOT NULL,
+  `date_of_birth` date NOT NULL,
+  `gender` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `is_admin` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `users`
+--
+
+INSERT INTO `users` (`id_user`, `name`, `number`, `date_of_birth`, `gender`, `email`, `password`, `is_admin`) VALUES
+(1, 'admin', '71996613066', '0000-00-00', 'Male', 'update@email.com', '1234', 1);
+
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices de tabela `address`
+--
+ALTER TABLE `address`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Índices de tabela `descriptions`
@@ -48,14 +123,76 @@ ALTER TABLE `descriptions`
   ADD PRIMARY KEY (`id_description`);
 
 --
+-- Índices de tabela `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`id_items`),
+  ADD KEY `fk` (`id_descriptionfk`);
+
+--
+-- Índices de tabela `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id_cart`),
+  ADD KEY `user_fk` (`id_userfk`),
+  ADD KEY `items_fk` (`id_itemsfk`);
+
+--
+-- Índices de tabela `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`);
+
+--
 -- AUTO_INCREMENT para tabelas despejadas
 --
+
+--
+-- AUTO_INCREMENT de tabela `address`
+--
+ALTER TABLE `address`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `descriptions`
 --
 ALTER TABLE `descriptions`
   MODIFY `id_description` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `items`
+--
+ALTER TABLE `items`
+  MODIFY `id_items` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id_cart` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `items`
+--
+ALTER TABLE `items`
+  ADD CONSTRAINT `fk` FOREIGN KEY (`id_descriptionfk`) REFERENCES `descriptions` (`id_description`);
+
+--
+-- Restrições para tabelas `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `items_fk` FOREIGN KEY (`id_itemsfk`) REFERENCES `items` (`id_items`),
+  ADD CONSTRAINT `user_fk` FOREIGN KEY (`id_userfk`) REFERENCES `users` (`id_user`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
